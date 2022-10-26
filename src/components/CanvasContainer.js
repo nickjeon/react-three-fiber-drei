8293b8948eac8b5adf3d15e27bfca065034c2ee0
nickjeon <nicholaskjeon@gmail.com>
2022-10-26 01:04:14 -0400

    initial commit

---
+++ src/components/CanvasContainer.js
@@ -0,0 +1,28 @@
+import { Canvas } from '@react-three/fiber';
+import {
+	PresentationControls,
+	Environment,
+	ContactShadows
+} from '@react-three/drei'
+
+import Watch from './Watch';
+
+export default function CanvasContainer() {
+  return (
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
+      <PresentationControls
+        global
+        config={{ mass: 2, tension: 500 }}
+        snap={{ mass: 4, tension: 1500 }}
+        rotation={[0, 0.3, 0]}
+        polar={[-Math.PI / 3, Math.PI / 3]}
+        azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
+        <Watch rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.25, 0]} scale={0.003} />
+      </PresentationControls>
+      <ContactShadows position={[0, -1.4, 0]} opacity={0.75} scale={10} blur={2.5} far={4} />
+      <Environment preset="city" />
+    </Canvas>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { Loader } from '@react-three/drei';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+    <Loader />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
